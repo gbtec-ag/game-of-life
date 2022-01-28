d3fa6896a0579768c6019035089c221710fe4f3b
jonathan.kampmann <jonathan.kampmann@gbtec.com>
2022-01-28 10:18:45 +0100

    feat(DES-2362): Add constructor for Generation,  and change type of cells to List<List<Cell>>

--- src/main/java/com/gbtec/model/Generation.java
+++ src/main/java/com/gbtec/model/Generation.java
@@ -2,20 +2,25 @@ package com.gbtec.model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import lombok.Data;
 
 @Data
 public class Generation {
 
-    private ArrayList<ArrayList<Cell>> cells;
+    private final List<List<Cell>> cells;
+
+    public Generation(List<List<Cell>> cells){
+        this.cells = cells;
+    }
 
     public Generation() {
         cells = new ArrayList<>();
     }
 
     public void addLine(String line) {
-        final var cellGrid = Arrays.stream(line.split("")).map(character -> character.equals("X")).map(Cell::new).toList();
+        final var cellGrid = Arrays.stream(line.split("")).map(character -> character.equals("1")).map(Cell::new).toList();
         cells.add(new ArrayList<>(cellGrid));
     }
 }
