7a2567ec321b68cdae377790f3f27bec80eb1e14
Nebi Kiracti <nebi.kiracti@gbtec.com>
2022-08-10 16:53:39 +0200

    feat(DES-3210): implementation of NextGeneration

--- src/main/java/com/gbtec/Main.java
+++ src/main/java/com/gbtec/Main.java
@@ -1,13 +1,14 @@
 package com.gbtec;
 
 public class Main {
-
+private static int i = 0;
     public static void main(String[] args) {
-
+        while(i<9){
         Generation generation = new Generation();
-
         generation.initialize();
         generation.printStatus();
+        System.out.println("");
+        i++;}
 
 
     }
