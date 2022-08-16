29f4d6f864a4450db2dd117caa44ddafcf905251
Nebi Kiracti <nebi.kiracti@gbtec.com>
2022-08-16 16:34:01 +0200

    refactor(DES-3210): Adding static method randomizer for random Gen.

---
+++ src/main/java/com/gbtec/GenerationRandomizer.java
@@ -0,0 +1,15 @@
+package com.gbtec;
+import java.util.Random;
+
+public class GenerationRandomizer {
+    public static Generation randomizeGeneration(Generation generation, int size)
+    {
+        Random random = new Random();
+        for (int y=0; y<size; y++){
+            for(int x=0; x<size;x++){
+                generation.generation.get(y).get(x).setStatus(random.nextBoolean());
+            }
+        }
+        return generation;
+    }
+}
