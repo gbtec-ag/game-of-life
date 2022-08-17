4386e786cc61f4bf21fb6b8fe18972a546cdd848
Nebi Kiracti <nebi.kiracti@gbtec.com>
2022-08-17 12:58:59 +0200

    refactor(DES-3210):Trying to override previous Generation in Console

--- src/main/java/com/gbtec/Main.java
+++ src/main/java/com/gbtec/Main.java
@@ -2,46 +2,53 @@ package com.gbtec;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         int sizeOfGeneration = 100;
         int numberOfNextGenerations = 100;
         int wishPattern = 2;
 
-        // Selecting wch kind of pattern you want
+
+        // Selecting which kind of pattern you want
         Generation generation = new Generation(sizeOfGeneration);
         if (wishPattern == 0) {
             GenerationRandomizer.randomizeGeneration(generation, sizeOfGeneration);
-            GenerationPrinter.printIntoConsole(generation);
+            //introducing the RealisticGenerationPrinter
+            RealsticGenerationPrinter printer2 = new RealsticGenerationPrinter();
+            printer2.printGeneration(generation,sizeOfGeneration);
 
             for (int i = 0; i <= numberOfNextGenerations - 1; i++) {
                 NextGeneration nextGeneration = new NextGeneration();
-                // Thread.sleep(500);
+                Thread.sleep(500);
                 generation = nextGeneration.calculateNextGeneration(generation, sizeOfGeneration);
-                GenerationPrinter.printIntoConsole(generation);
+                printer2.printGeneration(generation,sizeOfGeneration);
+
             }
         }
         // for createFlower, size of Generation should be greater than 100 or ==100
         else if (wishPattern == 1 && sizeOfGeneration >= 100) {
             numberOfNextGenerations = 54;
             GenerationPattern.createFlower(generation, sizeOfGeneration);
-            GenerationPrinter.printIntoConsole(generation);
+            RealsticGenerationPrinter printer2 = new RealsticGenerationPrinter();
+            printer2.printGeneration(generation,sizeOfGeneration);
 
             for (int i = 0; i <= numberOfNextGenerations - 1; i++) {
                 NextGeneration nextGeneration = new NextGeneration();
                 // Thread.sleep(500);
                 generation = nextGeneration.calculateNextGeneration(generation, sizeOfGeneration);
-                GenerationPrinter.printIntoConsole(generation);
+                printer2.printGeneration(generation,sizeOfGeneration);
+
             }
         } else if (wishPattern == 2) {
             GenerationPattern.createGlider(generation);
+            RealsticGenerationPrinter printer2 = new RealsticGenerationPrinter();
+            printer2.printGeneration(generation,sizeOfGeneration);
 
-            GenerationPrinter.printIntoConsole(generation);
 
             for (int i = 0; i <= numberOfNextGenerations - 1; i++) {
                 NextGeneration nextGeneration = new NextGeneration();
                 // Thread.sleep(500);
                 generation = nextGeneration.calculateNextGeneration(generation, sizeOfGeneration);
-                GenerationPrinter.printIntoConsole(generation);
+                printer2.printGeneration(generation,sizeOfGeneration);
             }
         } else {
             System.out.println("------");
---
+++ src/main/java/com/gbtec/RealsticGenerationPrinter.java
@@ -0,0 +1,56 @@
+package com.gbtec;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.fusesource.jansi.AnsiConsole;
+
+public class RealsticGenerationPrinter {
+
+    public static final int WAIT_MILLIS = 1000;
+
+    public RealsticGenerationPrinter() {
+        AnsiConsole.systemInstall();
+        System.out.println(ansi().eraseScreen().cursorMove(0, 0));
+    }
+
+    public void printDemo() throws InterruptedException {
+        System.out.println(ansi().eraseLine().cursorUpLine().render("@|yellow X|@ @|white X|@"));
+        Thread.sleep(WAIT_MILLIS);
+        System.out.println(ansi().eraseLine().cursorUpLine().render("@|white X|@ @|yellow X|@"));
+        Thread.sleep(WAIT_MILLIS);
+        System.out.println(ansi().eraseLine().cursorUpLine().render("@|yellow X|@ @|white X|@ █▄▀■░"));
+        Thread.sleep(WAIT_MILLIS);
+    }
+
+    public void printGeneration(Generation generation, int size) throws InterruptedException {
+       List<String> outputString = renderGeneration(generation, size);
+       int i = 0;
+       while(i<=size-1) {
+           System.out.println(ansi().render(outputString.get(i)));
+           i++;
+       }
+           Thread.sleep(WAIT_MILLIS);
+    }
+
+     private List<String> renderGeneration(Generation generation, int size) {
+        //Taking the two-dimensional List in order to get to the cells
+        List<List<Cell>> currentGeneration = generation.generation;
+        List<String> renderedGeneration = new ArrayList<>();
+        for (int i = 0; i <= size-1; i++) {
+            List<Cell> currentRow = currentGeneration.get(i);
+            String line="";
+            for (int j = 0; j <= size-1; j++) {
+                Cell currentCell = currentRow.get(j);
+                if (currentCell.getStatus()==false) {
+                    line = line + "@|yellow -|@";
+                } else {
+                    line = line + "@|white o|@";
+                }
+            }
+            renderedGeneration.add(line);
+        }
+        return renderedGeneration;
+    }
+}
