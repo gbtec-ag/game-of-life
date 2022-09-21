3de87157816807b686092ef7e55b9a392a7f6150
Igor Martens <igor.martens@gbtec.de>
2022-09-21 14:09:47 +0200

    feat(DES-3348): rename and reduce the entrypoint class

---
+++ src/main/java/com/gbtec/GameOfLifeApplication.java
@@ -0,0 +1,11 @@
+package com.gbtec;
+
+import org.springframework.boot.SpringApplication;
+
+public class GameOfLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameOfLifeApplication.class, args);
+    }
+
+}
