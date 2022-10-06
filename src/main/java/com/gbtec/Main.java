9c1b9f26e6e7b11d7cd706f66fb52401a7b3674f
Maximilian Logiewa <maximilian.logiewa@gbtec.com>
2022-10-06 12:02:25 +0200

    Print out grid

--- src/main/java/com/gbtec/Main.java
+++ src/main/java/com/gbtec/Main.java
@@ -1,9 +1,20 @@
 package com.gbtec;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        int[][] raster = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+        int i = 0;
+        int j = 0;
+        // int k = 0;
+        for(i = 0; i < raster.length; i++) {
+            for(j = 0; j < raster.length; j++) {
+                System.out.print(raster[i][j] + "  ");
+            }
+            System.out.println();
+        }
     }
 
 }
