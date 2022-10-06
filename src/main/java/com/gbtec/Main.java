6ac2603097d6497a58c95c38b10265d5434d1c45
Maximilian Logiewa <maximilian.logiewa@gbtec.com>
2022-10-06 11:01:03 +0200

    Clean up Main class

--- src/main/java/com/gbtec/Main.java
+++ src/main/java/com/gbtec/Main.java
@@ -1,22 +1,9 @@
 package com.gbtec;
 
-import static com.gbtec.service.GenerationCalculator.calculateNextGeneration;
-import static com.gbtec.service.GenerationReader.createInitialGeneration;
-
-import com.gbtec.model.Generation;
-import com.gbtec.presentation.Printer;
-
 public class Main {
 
     public static void main(String[] args) {
-        Printer printer = new Printer();
-        Generation currentGeneration = createInitialGeneration();
 
-        printer.printGeneration(currentGeneration);
-        while (true) {
-            currentGeneration = calculateNextGeneration(currentGeneration);
-            printer.printGeneration(currentGeneration);
-        }
     }
 
 }
