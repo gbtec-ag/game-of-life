3852e7a92109630e638043cd8ed0e5a7504358b0
Maximilian Logiewa <maximilian.logiewa@gbtec.com>
2022-10-14 12:18:33 +0200

    feat(DES-3382): Implementation of random grid generation & solution for execution stop after static grid (not fix for oscillators yet)

--- src/main/java/com/gbtec/gameoflife/implementation/GameOfLifeService.java
+++ src/main/java/com/gbtec/gameoflife/implementation/GameOfLifeService.java
@@ -5,65 +5,93 @@ import com.gbtec.gameoflife.implementation.MaxVersion.GameRules;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.Random;
+
 @Service
 public class GameOfLifeService extends GameOfLifeCommandProxy {
     // @formatter:off
     // Instead of "int[][]" you can also use "boolean[][]"
 
-    private int[][] generationData = new int[][]{
-            {0, 1, 0, 0, 0},
-            {0, 0, 1, 0, 0},
-            {1, 1, 1, 0, 0},
-            {0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0},
-    };
-
     /*
     private int[][] generationData = new int[][]{
-            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-            {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {0, 1, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
     };
     */
+
+    private int[][] generationData = new int[][]{
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    };
+
     // @formatter:on
     private int[][] firstGenerationData = generationData;
-    private boolean printRunning;
-    // private int equalIndex = 0;
+    private boolean printShouldRun = true;
+        // Random object for random generation
+    Random randomValues = new Random();
 
     public GameOfLifeService(SimpMessagingTemplate simpMessagingTemplate) {
         super(simpMessagingTemplate);
     }
 
     @Override
-    public void init() {
+    public void init(/* int y, int x*/) {
+
+        // for initiation through web client
+        /*
+        private int[][] generationData = new int[y][x];
+        */
+
+        // Predetermined grid is printed
         drawGeneration(firstGenerationData);
         generationData = firstGenerationData;
-    }
+
+        // Random generation
+        /*
+        for (int y = 0; y < generationData.length; y++) {
+            for (int x = 0; x < generationData.length; x++) {
+                // Storing random number either 0 or 1
+                int randomValueForGeneration = randomValues.nextInt(2);
+
+                // Matching random value to corresponding array index
+                firstGenerationData[y][x] = randomValueForGeneration;
+            }
+        }
         // @formatter:off
         /* To generate random values, you can use the class "java.util.Random". This class is providing two functions
          * which are really handy for our case: "nextBoolean" and "nextInt".
          * - "nextBoolean" will give you randomly "true" or "false"
          * - "nextInt" will give you a random int number. To limit te numbers given by this method to "0" and "1"
          *   you must set the origin to 0 and the bound to 2 like "nextInt(0, 2)"
-         */
+
         // @formatter:on
+        drawGeneration(firstGenerationData);
+        generationData = firstGenerationData;
+        // would be better placed in a single method with individual button -> separation of initiation and reset
+        */
+    }
 
     @Override
     public void next() {
@@ -76,23 +104,21 @@ public class GameOfLifeService extends GameOfLifeCommandProxy {
         /* Possibility that you also input if u want a toroid grid when calling the method (either 'play' or 'init'
         *  which would make more sense)
         */
-        printRunning = true;
-        while (printRunning) {
-            int[][] newGenerationData = GameRules.createNewGen(generationData); // checking the rules for next generation
-            drawGeneration(newGenerationData);         // printing the grid
+        printShouldRun = true;
+        while (printShouldRun) {
+            int[][] newGenerationData = GameRules.createNewGen(generationData);
+            drawGeneration(newGenerationData);
+
+            /* Check if old array is the same as the new -> would mean that it's a static field and should stop
+             * executing method 'play' to reduce computing power
+             */
+            if (Arrays.deepEquals(generationData, newGenerationData)) {
+                stop();
+            }
 
             generationData = newGenerationData;
 
-/*
-            for (int i = 0; i < newGenerationData.length - 1; i++) {
-                for (int j = 0; j < newGenerationData.length - 1; j++) {
-                    if (newGenerationData[i][j] != generationData[i][j]) {
-                        // action
-                    } else // stop();
-                }
-            }
-*/
-// alternative: play button after push into stop button to force action
+
 
             try {
                 Thread.sleep(delayMs);
@@ -100,10 +126,10 @@ public class GameOfLifeService extends GameOfLifeCommandProxy {
                 throw new RuntimeException(e);
             }
         }
-    }
+    }   // alternative: play button after push into stop button to force action
 
     @Override
     public void stop() {
-        printRunning = false;
+        printShouldRun = false;
     }
 }
