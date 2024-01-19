923b08c575d779af15f436b7a8a3b670387b1ef6
Ben <was.geht.dich.das.an@gmail.com>
2024-01-19 10:53:42 +0100

    Fertig

--- src/main/java/com/gbtec/gameoflife/implementation/GameOfLifeService.java
+++ src/main/java/com/gbtec/gameoflife/implementation/GameOfLifeService.java
@@ -5,6 +5,8 @@ import com.gbtec.gameoflife.framework.GameOfLifeCommandProxy;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class GameOfLifeService extends GameOfLifeCommandProxy {
 
@@ -12,6 +14,8 @@ public class GameOfLifeService extends GameOfLifeCommandProxy {
         super(simpMessagingTemplate);
     }
 
+    int[][] generationData;
+    int[][] tempGenerationData;
     @Override
     public void init() {
         // @formatter:off
@@ -29,7 +33,7 @@ public class GameOfLifeService extends GameOfLifeCommandProxy {
         // @formatter:on
 
         drawGeneration(generationData);
-
+        tempGenerationData = new int[generationData.length][generationData[0].length];
         // @formatter:off
         /* To generate random values, you can use the class "java.util.Random". This class is providing two functions
          * which are really handy for our case: "nextBoolean" and "nextInt".
@@ -38,5 +42,63 @@ public class GameOfLifeService extends GameOfLifeCommandProxy {
          *   you must set the origin to 0 and the bound to 2 like "nextInt(0, 2)"
          */
         // @formatter:on
+    }
+    public void next() {
+
+        for (int y = 0; y < generationData.length; y++) {
+            for (int x = 0; x < generationData[y].length; x++) {
+
+                int neighborCount = countNeighbors(y, x);
+
+                applyRules(neighborCount, y, x);
+            }
+        }
+        // other for loop bc all changes are done at this point, y and x are needed for copy
+        for (int y = 0; y < generationData.length; y++) {
+            for(int x = 0; x < generationData[y].length; x++){
+                generationData[y][x] = tempGenerationData[y][x];
+            }
+        }
+
+        drawGeneration(generationData);
+        
+    }
+    private int countNeighbors(int y, int x) {
+        int neighborCount = 0;
+
+        for (int r = -1; r <= 1; r++) {
+            for (int c = -1; c <= 1; c++) {
+
+                int neighborY = y + r;
+                int neighborX = x + c;
+
+                // prove if neigbor is in bounds
+                if (neighborY > -1 && neighborX > -1 && neighborY < generationData.length && neighborX < generationData.length) {
+                    // prove if neighbor is alive
+                    if (generationData[neighborY][neighborX] == 1) {
+                        if (r + c != 0){ // every neighbor beside [1][-1], [-1][1], [0][0]
+                            neighborCount++;
+                        } else if (r == -1 || r == 1) { // every neighbor beside [0][0] (not counts himself)
+                            neighborCount++;
+                        }
+                    }
+                }
+            }
+        }
+        return neighborCount;
+    }
+    private void applyRules(int neighborCount, int y, int x) {
+        // proving rules in generationData, applying in tempGenerationData
+        if(generationData[y][x] == 1) {
+            if(neighborCount <2) {
+                tempGenerationData[y][x] = 0;
+            } else if (neighborCount > 3) {
+                tempGenerationData[y][x] = 0;
+            } else if (neighborCount == 2 || neighborCount == 3) {
+                tempGenerationData[y][x] = 1;
+            }
+        } else if (generationData[y][x] == 0 && neighborCount == 3) {
+            tempGenerationData[y][x] = 1;
+        }
     }
 }
